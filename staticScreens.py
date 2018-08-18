bac7b3e7272369f560827bdcb1c9ea01743081d0
Romain <romain.mendez.rm@gmail.com>
2018-08-18 13:54:15 +0200

    staticScreens, renamed and added background for game

--- staticScreens.py
+++ staticScreens.py
@@ -6,3 +6,11 @@ class HomeScreen(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = 0
         self.rect.y = 0
+
+class MapImage(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("ressources/map.jpg")
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        self.rect.y = 0
