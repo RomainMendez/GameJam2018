f852d6f6869fe16b7a07a41347ed8180931b9e36
Alban <fav.alban@gmail.com>
2018-08-18 13:39:18 +0200

    world update

--- world.py
+++ world.py
@@ -1,8 +1,14 @@
 import pygame
+import visualmap
 
 class World():
-    def __init__(self, sprite_list):
-        self.all_list = sprite_list
+    def __init__(self,sprite_list,screen):
+        self.all_Sprite=sprite_list
+        self.actual_map=visualmap.Map(self.all_Sprite,screen)
+
+    def update(self)
+        self.all_Sprite.update()
+
 
 
 
