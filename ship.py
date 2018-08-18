d404f2fc37a00962ba2e9b074a1a66027185c42c
Romain <romain.mendez.rm@gmail.com>
2018-08-18 16:06:12 +0200

    ship : added decceleration

--- ship.py
+++ ship.py
@@ -1,5 +1,6 @@
 class Ship():
-    def __init__(self, max_speed, cargo, acceleration):
+    def __init__(self, max_speed, cargo, acceleration, decceleration):
         self.max_speed = max_speed
         self.cargo = cargo
         self.acceleration = acceleration
+        self.decceleration = decceleration
