057ef7560974ea7d1c2341653434eec3d7ce1267
Romain <romain.mendez.rm@gmail.com>
2018-08-18 14:29:56 +0200

    ship: created staticscreens (interesting stuff in comments)

---
+++ ship.py
@@ -0,0 +1,5 @@
+class Ship():
+    def __init__(self, max_speed, cargo, acceleration):
+        self.max_speed = max_speed
+        self.cargo = cargo
+        self.acceleration = acceleration
--- staticScreens.py
+++ staticScreens.py
@@ -3,6 +3,7 @@ class HomeScreen(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load("ressources/homescreen.jpg")
+        #self.image = pygame.transform.scale(self.image, (640, 360))
         self.rect = self.image.get_rect()
         self.rect.x = 0
         self.rect.y = 0
