4484aa9a04f66db7ec9c4d0601f734e935e00d99
Romain <romain.mendez.rm@gmail.com>
2018-08-18 13:22:46 +0200

    homeScreen.py added and completed

---
+++ homeScreen.py
@@ -0,0 +1,8 @@
+import pygame
+class HomeScreen(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("ressources/homescreen.jpg")
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        self.rect.y = 0
