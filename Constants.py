1cb4fde589eb46b41db04503f0ef09d71c0d69b3
Alban <fav.alban@gmail.com>
2018-08-18 18:40:52 +0200

    constante et naval object

--- Constants.py
+++ Constants.py
@@ -5,7 +5,13 @@ def BAR_HEIGHT():
     return 80
 
 def GAME_AREA_HEIGHT():
-    return 720 - BAR_HEIGHT()
+    return SCREEN_HEIGHT() - BAR_HEIGHT()
+
+def GAME_AREA_WIDTH():
+    return SCREEN_WIDTH() - TWO()*COAST_OFFSET()
+
+def COAST_OFFSET():
+    return 91
 
 def SCREEN_WIDTH():
     return 1280
@@ -15,3 +21,6 @@ def BASIC_ENNEMY_DETECTION_RANGE():
 
 def BASIC_ENNEMY_LOSING_RANGE():
     return 40*40
+
+def TWO():
+    return 2
