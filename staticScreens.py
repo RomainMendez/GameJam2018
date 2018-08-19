68caf394281ed5a9ef022ed1b65c1f63ce8d2e88
Romain <romain.mendez.rm@gmail.com>
2018-08-19 09:20:38 +0200

    gameOver screen added

--- staticScreens.py
+++ staticScreens.py
@@ -15,3 +15,12 @@ class MapImage(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = 0
         self.rect.y = 0
+        
+class GameOver(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("ressources/gameover.jpg")
+        self.image = pygame.transform.scale(self.image, (1280, 720))
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        self.rect.y = 0
