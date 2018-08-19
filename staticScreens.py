355a106d58af91a9b0e866e8e1b63e394cb852a8
Romain <romain.mendez.rm@gmail.com>
2018-08-19 15:41:09 +0200

    Changed background image

--- staticScreens.py
+++ staticScreens.py
@@ -13,7 +13,7 @@ class HomeScreen(pygame.sprite.Sprite):
 class MapImage(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("ressources/map.jpg")
+        self.image = pygame.image.load("ressources/map.png")
         self.rect = self.image.get_rect()
         self.rect.x = 0
         self.rect.y = 0
